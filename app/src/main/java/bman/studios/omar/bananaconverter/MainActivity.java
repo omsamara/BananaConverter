1565e555dc4cb0664c877faa15bd2129a2de6254
Omar Samara <omsamara@ncsu.edu>
2017-04-19 00:45:33 -0400

    Added class variable comments

--- app/src/main/java/bman/studios/omar/bananaconverter/MainActivity.java
+++ app/src/main/java/bman/studios/omar/bananaconverter/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView image;
 
-    /** Variable for the popup message that shows conversion */
+    /** Variable for the popup message that shows conersion */
     Toast mToast;
 
     @Override
